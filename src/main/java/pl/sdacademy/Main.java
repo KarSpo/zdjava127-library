0c62f8e7d6b651efa23bd60ac02c2a210d1aac83
MichalMus <michalmusialski@wp.pl>
2023-01-02 21:31:28 +0100

    dodanie metody sprawdzającej istnienie numeru inwentaryzacyjnego w Encji BookRegistration

--- src/main/java/pl/sdacademy/Main.java
+++ src/main/java/pl/sdacademy/Main.java
@@ -1,8 +1,22 @@
 package pl.sdacademy;
 
 
+import javax.persistence.EntityManager;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
 
+  }
+  public static void isRegistrationNumberexist() {
+    System.out.println("Podaj nr do sprawdzenia: ");
+    Scanner scanner = new Scanner(System.in);
+    String numberToCheck = scanner.nextLine();
+    EntityManager entityManager = HibernateUtil.getSessionFactory().createEntityManager();
+    entityManager.getTransaction().begin();
+    BookRegistration r = entityManager.createQuery("from BookRegistration br where br.registrationNumber = :n", BookRegistration.class)
+            .setParameter("n", numberToCheck)
+            .getSingleResult();
+    System.out.println(r);
   }
 }
